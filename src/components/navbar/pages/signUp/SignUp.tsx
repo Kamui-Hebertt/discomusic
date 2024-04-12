ef419e56cabe6f083e7dc48838967fe59d07a532
Hebertt Nascimento <103397763+Kamui-Hebertt@users.noreply.github.com>
2024-04-12 03:46:12 -0300

    feat: register user integration

--- src/components/navbar/pages/signUp/SignUp.tsx
+++ src/components/navbar/pages/signUp/SignUp.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import './SignUpPage.css';
+import axios from 'axios';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { toast } from 'react-toastify'
 
 const SignUp: React.FC = () => {
   const [formData, setFormData] = useState({
@@ -8,6 +12,7 @@ const SignUp: React.FC = () => {
     email: '',
     password: '',
   });
+  const [loading, setLoading] = useState(false); // State to track loading status
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -17,32 +22,108 @@ const SignUp: React.FC = () => {
     }));
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     console.log('Form submitted:', formData);
-    // You can add form submission logic here, like sending data to a server
-  };
+    try {
+      if (
+        formData.firstName.trim() === '' ||
+        formData.nickname.trim() === '' ||
+        formData.email.trim() === '' ||
+        formData.password.trim() === ''
+      ) {
+        throw new Error('Os campos não podem estar em branco.');
+      }
+    
+      setLoading(true); // Start loading
+    
+      const response = await axios.post(
+        'https://discomusic-serverside.onrender.com/api/registerNewUser',
+        {
+          name: formData.firstName,
+          nickname: formData.nickname,
+          email: formData.email,
+          password: formData.password,
+        }
+      );
+    
+      console.log(response);
+      if (response.status === 201 || response.status === 200) {
+        toast.success('Conta criada com sucesso!');
+        setTimeout(() => {
+          window.location.href = '/login'; 
+        }, 3000);
+
+      } else {
+        throw new Error('Não foi possível registrar a conta.');
+      }
+    } catch (error) {
+      console.error("Não foi possível registrar a conta:", error);
+      if (axios.isAxiosError(error) && error.response && error.response.data && error.response.data.error) {
+        toast.error(error.response.data.error as string);
+      } else {
+        toast.error("Ocorreu um erro ao processar a solicitação.");
+      }
+    } finally {
+      setLoading(false); // Stop loading
+    }
+
+  }
 
   return (
-    <div className="signup-container">
-      <h2>Crie Uma Conta</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} placeholder="Nome" required />
-        </div>
-        <div className="form-group">
-          <input type="text" name="nickname" value={formData.nickname} onChange={handleChange} placeholder="Nickname" required />
-        </div>
-        <div className="form-group">
-          <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" required />
-        </div>
-        <div className="form-group">
-          <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Senha" required />
-        </div>
-        <button type="submit">Registrar</button>
-      </form>
-    </div>
+    <>
+      <ToastContainer />
+      <div className="signup-container">
+        <h2>Crie Uma Conta</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              placeholder="Nome"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="nickname"
+              value={formData.nickname}
+              onChange={handleChange}
+              placeholder="Nickname"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Senha"
+              required
+            />
+          </div>
+          <button type="submit" className={loading ? "loading-button" : ""}>
+            {loading ? "Carregando..." : "Registrar"}
+          </button>
+        </form>
+      </div>
+    </>
   );
-}
+};
+
 
 export default SignUp;
